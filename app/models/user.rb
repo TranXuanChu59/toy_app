0bcb98deb673f02d85c37a1d937b10a3c04f898c
TranXuanChu <tran.xuan.chu@framgia.com>
2018-08-10 08:28:25 +0700

    Finish toy app

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :microposts
+  validates :name, length: { maximum: 100 },
+                      presence: true
+  validates :email, length: { in: 4..100 },
+                      presence: true
 end
